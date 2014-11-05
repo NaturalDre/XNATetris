c88415ebbdd91a49654ab472349ba0a5b59d755b
Andre M <naturaldre@gmail.com>
2014-11-04 20:42:11 -0500

    Move Sprite classes to BytesOfPi.Graphics namespace.

--- tetris/tetris/AnimatedSprite.cs
+++ tetris/tetris/AnimatedSprite.cs
@@ -6,7 +6,7 @@ using System.Diagnostics;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
-namespace Tetris
+namespace BytesOfPi.Graphics
 {
     class AnimatedSprite : ISprite
     {
--- tetris/tetris/ISprite.cs
+++ tetris/tetris/ISprite.cs
@@ -6,7 +6,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 
-namespace Tetris
+namespace BytesOfPi.Graphics
 {
     interface ISprite
     {
